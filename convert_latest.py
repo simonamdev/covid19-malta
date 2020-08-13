5a56e9628df58df15d88fc7270aefd7fcf99f1a9
Simon Agius Muscat <simon@agius-muscat.net>
2020-08-13 21:23:17 +0200

    Completed conversion script

--- convert_latest.py
+++ convert_latest.py
@@ -1,8 +1,9 @@
 import os
+import json
 
 files = os.listdir("data")
-files = sorted([file for file in files if not file ==
-                '.gitkeep'], reverse=True)
+files = sorted(
+    [file for file in files if file.endswith('csv')], reverse=True)
 if len(files) == 0:
     print('Error: no files found')
     exit(1)
@@ -18,8 +19,17 @@ with open(f'./data/{latest_file}', 'r') as file:
         date, year, new_cases, total_cases, recovered, deaths, active_cases = line.strip().split(',')
         output_data.append(
             {
-                'date': f'{date}-{year}'
+                'date': f'{date}-{year}',
+                'new_cases': int(new_cases),
+                'total_cases': int(total_cases),
+                'recovered': int(recovered),
+                'deaths': int(deaths),
+                'active_cases': int(active_cases)
             }
         )
 
 print(output_data)
+output_file_path = './data/latest_data.json'
+
+with open(output_file_path, 'w') as file:
+    file.write(json.dumps(output_data, indent=2))
