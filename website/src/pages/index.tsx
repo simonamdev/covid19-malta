ba5d572159e2923afa97d8b1052716eb4f53d3c7
Simon Agius Muscat <simon@agius-muscat.net>
2020-10-06 22:25:18 +0200

    Added last updated time

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -71,6 +71,7 @@ export default (props: IndexPageProps) => {
     <h1>Covid-19 in Malta</h1>
     <div>
       <p>Website by <a href="https://simonam.dev">Simon Agius Muscat</a>. Data retrieved from the <a href="https://github.com/COVID19-Malta/COVID19-Cases">Public Health Open Dataset</a></p>
+      <p>Last updated: {new Date().toTimeString()}</p>
     </div>
     <div style={{ height: '80vh' }}>
       <ControllableMultipleCountChart countChartData={data} measuresData={parsedMeasuresData} />
