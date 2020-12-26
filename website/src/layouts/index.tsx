ad6f0b0f51107e7f85f6c6ca6b917303ac85d440
Simon Agius Muscat <simon@agius-muscat.net>
2020-12-26 18:41:50 +0100

    Refactor React Helmet

--- website/src/layouts/index.tsx
+++ website/src/layouts/index.tsx
@@ -1,29 +1,29 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import * as React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import './index.css'
+import "./index.css";
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: "rebeccapurple",
+      marginBottom: "1.45rem",
     }}
   >
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: "1.45rem 1.0875rem",
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            color: "white",
+            textDecoration: "none",
           }}
         >
           Gatsby
@@ -31,40 +31,38 @@ const Header = () => (
       </h1>
     </div>
   </div>
-)
+);
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   location: {
-    pathname: string
-  }
-  children: any
+    pathname: string;
+  };
+  children: any;
 }
 
 class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
   public render() {
     return (
       <div>
-        <Helmet
-          title="C19"
-          meta={[
-            { name: 'description', content: 'C19' },
-            { name: 'keywords', content: 'Covid, Malta' },
-          ]}
-        />
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>C19 - Malta`</title>
+          <link rel="canonical" href="https://www.c19.mt" />
+        </Helmet>
         <Header />
         <div
           style={{
-            margin: '0 auto',
+            margin: "0 auto",
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
+            padding: "0px 1.0875rem 1.45rem",
             paddingTop: 0,
           }}
         >
           {this.props.children()}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default DefaultLayout
+export default DefaultLayout;
