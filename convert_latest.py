e85e087e63903f0a41917e345d8715462f90c551
Simon Agius Muscat <simon@agius-muscat.net>
2021-01-01 22:59:46 +0100

    Modified download and convert script to retrieve and parse swab count and positivity rate

--- convert_latest.py
+++ convert_latest.py
@@ -3,11 +3,19 @@ import json
 
 files = os.listdir("data")
 files = sorted(
-    [file for file in files if file.endswith('csv')], reverse=True)
+    [file for file in files if file.endswith('csv') and not 'swabs' in file], reverse=True)
 if len(files) == 0:
     print('Error: no files found')
     exit(1)
 latest_file = files[0]
+
+files = os.listdir("data")
+files = sorted(
+    [file for file in files if file.endswith('csv') and 'swabs' in file], reverse=True)
+if len(files) == 0:
+    print('Error: no swab files found')
+    exit(1)
+latest_swabs_file = files[0]
 
 
 def extract_seven_day_average(last_seven_days):
@@ -38,6 +46,40 @@ with open(f'./data/{latest_file}', 'r') as file:
         if len(last_seven_days) > 7:
             last_seven_days.pop(0)
         output_data.append(data)
+
+month_number_name_map = {
+    '01': 'Jan',
+    '02': 'Feb',
+    '03': 'Mar',
+    '04': 'Apr',
+    '05': 'May',
+    '06': 'Jun',
+    '07': 'Jul',
+    '08': 'Aug',
+    '09': 'Sep',
+    '10': 'Oct',
+    '11': 'Nov',
+    '12': 'Dec'
+}
+
+# Get swabs data and append
+first_line_skipped = False
+with open(f'./data/{latest_swabs_file}', 'r') as file:
+    for line in file:
+        if not first_line_skipped:
+            first_line_skipped = True
+            continue
+        entity, isocode, date, source_url, change_cumulative_total, _, _, _, _ = line.strip().split(',')
+        day, month, year = date.split(' ')[0].split('/')
+        month_name = month_number_name_map[month]
+        formatted_date = f'{day}-{month_name}-{year}'
+        for i, data in enumerate(output_data):
+            if data['date'] == formatted_date:
+                new_case_count = output_data[i]["new_cases"]
+                output_data[i]['swab_count'] = int(change_cumulative_total)
+                output_data[i]['positivity_rate'] = (
+                    new_case_count / int(change_cumulative_total)) * 100.0
+
 
 print(output_data)
 output_file_path = './data/latest_data.json'
--- download_latest.py
+++ download_latest.py
@@ -2,15 +2,28 @@ import requests
 
 from datetime import datetime
 
-url = 'https://raw.githubusercontent.com/COVID19-Malta/COVID19-Cases/master/COVID-19%20Malta%20-%20Aggregate%20Data%20Set.csv'
+cases_url = 'https://raw.githubusercontent.com/COVID19-Malta/COVID19-Cases/master/COVID-19%20Malta%20-%20Aggregate%20Data%20Set.csv'
 
-print('Downloading...')
-response = requests.get(url)
+print('Downloading Case Data...')
+response = requests.get(cases_url)
 
 if response.status_code == 200:
     filename = '{0:%Y-%m-%d_%H:%M:%S}_data.csv'.format(datetime.now())
     with open(f'./data/{filename}', 'w') as file:
         file.write(response.text)
-    print('Done :)')
 else:
-    print(f'Non 200 status code: {response.status_code}')
+    print(f'Non 200 status code for case data: {response.status_code}')
+
+swabs_url = 'https://raw.githubusercontent.com/COVID19-Malta/COVID19-Cases/master/COVID-19%20Malta%20-%20Number%20of%20PCR%20Tests%20by%20Date.csv'
+
+print('Downloading Swabs...')
+response = requests.get(swabs_url)
+
+if response.status_code == 200:
+    filename = '{0:%Y-%m-%d_%H:%M:%S}_swabs_data.csv'.format(datetime.now())
+    with open(f'./data/{filename}', 'w') as file:
+        file.write(response.text)
+else:
+    print(f'Non 200 status code for case data: {response.status_code}')
+
+print('Done :)')
