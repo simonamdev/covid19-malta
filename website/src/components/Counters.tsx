f9db9f928be9defa4941d2868b8be22457b733d2
Simon Agius Muscat <simon@agius-muscat.net>
2021-03-10 17:55:00 +0100

    Fixed Counters

--- website/src/components/Counters.tsx
+++ website/src/components/Counters.tsx
@@ -1,22 +1,41 @@
-import React from 'react'
-import { LineSeriesPoint } from 'react-vis';
+import React from "react";
+import { LineSeriesPoint } from "react-vis";
 
 interface CountersProps {
-    activeCasesData: LineSeriesPoint[];
-    deathsData: LineSeriesPoint[];
+  activeCasesData: LineSeriesPoint[];
+  deathsData: LineSeriesPoint[];
 }
 
 export default ({ activeCasesData, deathsData }: CountersProps) => {
-    const diffCount = activeCasesData[activeCasesData.length - 1].y - activeCasesData[activeCasesData.length - 2].y;
-    const isUp = diffCount > 0;
-    const diffDeathCount = deathsData[deathsData.length - 1].y - deathsData[deathsData.length - 2].y;
-    const deathsIsUp = diffDeathCount > 0;
-    return (
-        <div style={{ display: 'flex', flexDirection: 'row', width: '100%', }}>
-            <h3>{activeCasesData[activeCasesData.length - 1].y.toLocaleString()} Active Cases</h3>
-            <h4>, {Math.abs(diffCount)} {isUp ? 'more' : 'less'} case{Math.abs(diffCount) > 1 ? 's' : ''} than yesterday.</h4>{' '}
-            <h3 style={{ paddingLeft: '0.25em' }}>{deathsData[deathsData.length - 1].y} Deaths</h3>
-            <h4>, {Math.abs(diffDeathCount)} {deathsIsUp ? 'more' : 'less'} death{Math.abs(diffDeathCount) > 1 ? 's' : ''} than yesterday.</h4>
-        </div>
-    )
-}
+  const diffCount =
+    activeCasesData[activeCasesData.length - 1].y -
+    activeCasesData[activeCasesData.length - 2].y;
+  const casesAreUp = diffCount > 0;
+  const casesAreSame = diffCount === 0;
+  const diffDeathCount =
+    deathsData[deathsData.length - 1].y - deathsData[deathsData.length - 2].y;
+  const deathsIsUp = diffDeathCount > 0;
+  const deathsIsSame = diffDeathCount === 0;
+  return (
+    <div style={{ display: "flex", flexDirection: "row", width: "100%" }}>
+      <h3>
+        {activeCasesData[activeCasesData.length - 1].y.toLocaleString()} Active
+        Cases{" "}
+        {!casesAreSame && (
+          <span>
+            ({casesAreUp ? "⬆" : "⬇"} {Math.abs(diffCount)})
+          </span>
+        )}
+        ,
+      </h3>
+      <h3 style={{ paddingLeft: "0.25em" }}>
+        {deathsData[deathsData.length - 1].y} Deaths{" "}
+        {!deathsIsSame && (
+          <span>
+            ({deathsIsUp ? "⬆" : "⬇"} {Math.abs(diffDeathCount)})
+          </span>
+        )}
+      </h3>
+    </div>
+  );
+};
